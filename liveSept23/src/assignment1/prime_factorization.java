282fd940a2cb4c117fcc7b141b1459ac6d5df137
abhinav <abhinav@abhinav-HP-240-G3-Notebook-PC>
2023-09-21 21:47:43 +0530

    updated problem

--- liveSept23/src/assignment1/prime_factorization.java
+++ liveSept23/src/assignment1/prime_factorization.java
@@ -1,3 +1,20 @@
+/*
+Help Lakshya bhaiya do prime factorization!!
+Input Format
+
+Take an input number num.
+Constraints
+n<=100,000
+
+Output Format
+Print its prime factors in a single line
+
+Sample Input
+12
+
+Sample Output
+2 2 3
+ */
 package assignment1;
 import java.util.*;
 
@@ -9,7 +26,7 @@ public class prime_factorization {
 		int n = sc.nextInt();
 		int i=2;
 		
-		while(i<n) {
+		while(i<=n) {
 		
 			while(n%i==0) {
 				System.out.print(i+" ");
--- liveSept23/src/assignment1/print_series.java
+++ liveSept23/src/assignment1/print_series.java
@@ -1,3 +1,33 @@
+/*
+Take the following as input.
+
+A number (N1)
+A number (N2)
+Write a function which prints first N1 terms of the series 3n + 2 which are not multiples of N2.
+Input Format
+Constraints
+0 < N1 < 100 0 < N2 < 100
+Output Format
+
+Sample Input
+10 
+4
+
+Sample Output
+5 
+11 
+14 
+17 
+23 
+26 
+29 
+35 
+38 
+41
+
+Explanation
+The output will've N1 terms which are not divisible by N2. 
+ */
 package assignment1;
 import java.util.*;
 
@@ -9,9 +39,16 @@ public class print_series {
 		int N1 = sc.nextInt();
 		int N2 = sc.nextInt();
 		
-		int i=1;
-		while(N1>0) {
-			
+		int i=1,j=0;
+		while(j<N1) {
+			if(((3*i)+2)%N2 != 0) {
+				System.out.println((3*i)+2);
+			}
+			if(((3*i)+2)%N2 == 0) {
+				j--;
+			}
+			i++;
+			j++;
 		}
 	}
 
