57189ad0f238a6cf195ac240962ac7c069d07d8a
abhinav <abhinav@abhinav-HP-240-G3-Notebook-PC>
2023-10-03 16:07:19 +0530

    rev pattern

--- liveSept23/src/pattern/pattern_q31.java
+++ liveSept23/src/pattern/pattern_q31.java
@@ -1,28 +1,24 @@
-/*
-5
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1 
- */
 package pattern;
 import java.util.*;
 
-public class pattern_q30 {
+public class pattern_q31 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int row=n,col;
-		while(row>=1) {
+		int row=1, col;
+		while(row<=n) {
 			col=n;
-			while(col>=1) { 
-				System.out.print(col+" ");
+			while(col>=1) {
+				if(row!=col) {
+					System.out.print(col+" ");
+				} else {
+					System.out.print("* ");
+				}
 				col--;
 			}
-			row--;
+			row++;
 			System.out.println();
 		}
 	}
